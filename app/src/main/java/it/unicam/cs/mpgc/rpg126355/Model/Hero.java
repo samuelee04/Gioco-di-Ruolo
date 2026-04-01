b369276428fbb035b59ff9bc4cc6ff8ae8300938
BlitzLele <samu.sorr04@gmail.com>
2026-04-01 09:28:19 +0200

    Implementazione entità Hero con mapping JPA e generazione statistiche base

--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Hero.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Hero.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Random;
 
 @Entity
 public class Hero implements Serializable {
@@ -14,4 +15,99 @@ public class Hero implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private int hp;
+
+    @Column(nullable = false)
+    private int atk;
+
+    @Column(nullable = false)
+    private int def;
+
+    @Column(nullable = false)
+    private int exp;
+
+    protected Hero() {}
+
+    public Hero(String name) {
+        if(name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Il nome non può essere nullo o vuoto");
+        }
+        this.name = name;
+        this.exp = 10;
+
+        Random random = new Random();
+
+        this.hp = random.nextInt(41) + 80;
+        this.atk = random.nextInt(11) + 10;
+        this.def = random.nextInt(11) + 10;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Hero t)) {
+            return false;
+        }
+        return id != null && id.equals(t.id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
