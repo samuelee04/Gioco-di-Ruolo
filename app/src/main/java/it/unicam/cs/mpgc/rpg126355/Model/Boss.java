7cd682b3a6e1d0cd1e5310919e3b31b220c8a4d3
BlitzLele <samu.sorr04@gmail.com>
2026-04-01 09:54:03 +0200

    Implementazione entità Enemy.java e Boss.java con mapping JPA e generazione statistiche base.

--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Boss.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Boss.java
@@ -14,4 +14,80 @@ public class Boss implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int hp;
+
+    @Column(nullable = false)
+    private int atk;
+
+    @Column(nullable = false)
+    private int def;
+
+    @Column(nullable = false)
+    private int level;
+
+    protected Boss() {};
+
+    public Boss(String name, int hp, int atk, int def,int level){
+        if(name == null || name.isBlank() || hp < 0 || atk < 0 || def < 0 || level < 0){
+            throw new IllegalArgumentException("Ci sono errori nel costruttore");
+        }
+        this.name = name;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+        this.level = level;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Enemy.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Enemy.java
@@ -14,4 +14,68 @@ public class Enemy implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int hp;
+
+    @Column(nullable = false)
+    private int atk;
+
+    @Column(nullable = false)
+    private int def;
+
+    protected Enemy() {}
+
+    public Enemy(String name, int hp, int atk, int def){
+        if(name == null || name.isBlank() || hp < 0 || atk < 0 || def < 0){
+            throw new IllegalArgumentException("Ci sono errori nel costruttore");
+        }
+        this.name = name;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
 }
