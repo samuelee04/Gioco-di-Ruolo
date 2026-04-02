1906d475dad1d3bbc086e38666f735de857e04e7
BlitzLele <samu.sorr04@gmail.com>
2026-04-02 12:28:19 +0200

    Implementazione AbstractController con gestione SessionFactory e aggiunta BasicController

--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/AbstractController.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/AbstractController.java
@@ -3,12 +3,25 @@ package it.unicam.cs.mpgc.rpg126355.Controller;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 
 
 public abstract class AbstractController<T> implements Controller<T> {
 
 
     protected static SessionFactory sessionFactory;
+
+
+    private final Class<T> entityClass;
+
+
+    protected AbstractController(Class<T> entityClass) {
+        this.entityClass = entityClass;
+        if (sessionFactory == null) {
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        }
+    }
+
 
     @Override
     public void add(T entity) {
--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/BasicController.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/BasicController.java
@@ -1,3 +1,9 @@
 package it.unicam.cs.mpgc.rpg126355.Controller;
 
+public class BasicController<T> extends AbstractController<T>{
 
+
+    public BasicController(Class<T> entityClass) {
+        super(entityClass);
+    }
+}
