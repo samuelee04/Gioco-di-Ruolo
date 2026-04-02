22cfca6f422b4351534def8a58898e3229293437
BlitzLele <samu.sorr04@gmail.com>
2026-04-02 11:26:17 +0200

    Implementazione entità Weapon con mapping JPA e validazione dati

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Weapon.java
@@ -0,0 +1,68 @@
+package it.unicam.cs.mpgc.rpg126355.Model;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+public class Weapon implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int power;
+
+    @Column(nullable = false)
+    private int level;
+
+    protected Weapon() {}
+
+    public Weapon(String name, int power, int level){
+        if(name == null || name.isBlank() || power < 0 || level < 0){
+            throw new IllegalArgumentException("Ci sono errori nel costruttore");
+        }
+        this.name = name;
+        this.power = power;
+        this.level = level;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
