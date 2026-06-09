ffad085917c05b78c786c4629955b99d047eecf0
BlitzLele <samu.sorr04@gmail.com>
2026-06-09 16:59:06 +0200

    Completate operazioni CRUD in AbstractController e nell'interfaccia Controller

--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/AbstractController.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/AbstractController.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import java.util.List;
 
 
 public abstract class AbstractController<T> implements Controller<T> {
@@ -29,6 +30,33 @@ public abstract class AbstractController<T> implements Controller<T> {
             Transaction tx = session.beginTransaction();
             session.save(entity);
             tx.commit();
+        }
+    }
+
+    @Override
+    public void remove(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            T entity = session.find(entityClass, id);
+            session.delete(entity);
+            tx.commit();
+        }
+    }
+
+    @Override
+    public void update(T entity) {
+        try (Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.merge(entity);
+            session.getTransaction().commit();
+        }
+    }
+
+
+    @Override
+    public List<T> getAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from " + entityClass.getSimpleName(), entityClass).list();
         }
     }
 }
--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/Controller.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Controller/Controller.java
@@ -1,7 +1,14 @@
 package it.unicam.cs.mpgc.rpg126355.Controller;
 
+import java.util.List;
+
 public interface Controller<T> {
 
     void add(T element);
 
+    void remove(Long id);
+
+    void update(T entity);
+
+    List<T> getAll();
 }
