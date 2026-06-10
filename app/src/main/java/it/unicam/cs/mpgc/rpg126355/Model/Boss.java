1be7d213e54cb06a5f11c668d2d294e6181e753d
BlitzLele <samu.sorr04@gmail.com>
2026-06-10 12:34:25 +0200

    Refactoring strutturale e pulizia del codice in Boss.java

--- app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Boss.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126355/Model/Boss.java
@@ -11,6 +11,14 @@ public class Boss implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    private static final int MIN_HP    = 150, HP_RANGE    = 100;
+    private static final int MIN_ATK   = 15,  ATK_RANGE   = 20;
+    private static final int MIN_DEF   = 10,  DEF_RANGE   = 15;
+    private static final int MIN_LEVEL = 20,  LEVEL_RANGE = 10;
+
+    private static final String[] NOMI   = {"Azazel", "Gorgoroth", "Malekith", "Voldor", "Kael'thas", "Nyarlathotep", "Balthazar", "Sauron", "Morok"};
+    private static final String[] TITOLI = {"Il Distruttore", "Il Senzanome", "Signore dell'Abisso", "Re dei Lich", "Flagello degli Dei", "L'Ombra Strisciante", "Il Divoratore di Anime", "Il Titano Caduto", "L'Araldo della Rovina"};
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -30,12 +38,25 @@ public class Boss implements Serializable {
     @Column(nullable = false)
     private int level;
 
-    protected Boss() {};
+    protected Boss() {}
 
     public Boss(String name, int hp, int atk, int def,int level){
-        if(name == null || name.isBlank() || hp < 0 || atk < 0 || def < 0 || level < 0){
-            throw new IllegalArgumentException("Ci sono errori nel costruttore");
+        if (name == null || name.isBlank()){
+            throw new IllegalArgumentException("Il nome non può essere nullo o vuoto");
         }
+        if (hp < 0) {
+            throw new IllegalArgumentException("Gli HP non possono essere negativi");
+        }
+        if (atk < 0) {
+            throw new IllegalArgumentException("L'attacco non può essere negativo");
+        }
+        if (def < 0) {
+            throw new IllegalArgumentException("La difesa non può essere negativa");
+        }
+        if (level < 0) {
+            throw new IllegalArgumentException("Il livello non può essere negativo");
+        }
+
         this.name = name;
         this.hp = hp;
         this.atk = atk;
@@ -45,10 +66,6 @@ public class Boss implements Serializable {
 
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -94,20 +111,31 @@ public class Boss implements Serializable {
     public static Boss generateRandomBoss() {
         java.util.Random rand = new java.util.Random();
 
-        String[] nomi = {"Azazel", "Gorgoroth", "Malekith", "Voldor", "Kael'thas", "Nyarlathotep", "Balthazar", "Sauron", "Morok"};
-        String[] titoli = {
-                "Il Distruttore", "Il Senzanome", "Signore dell'Abisso", "Re dei Lich",
-                "Flagello degli Dei", "L'Ombra Strisciante", "Il Divoratore di Anime",
-                "Il Titano Caduto", "L'Araldo della Rovina"
-        };
+        String randomName = NOMI[rand.nextInt(NOMI.length)] + ", " + TITOLI[rand.nextInt(TITOLI.length)];
 
-        String randomName = nomi[rand.nextInt(nomi.length)] + ", " + titoli[rand.nextInt(titoli.length)];
-
-        int hp = rand.nextInt(100) + 150; // HP tra 150 e 249
-        int atk = rand.nextInt(20) + 15;  // ATK tra 15 e 34
-        int def = rand.nextInt(15) + 10;  // DEF tra 10 e 24
-        int level = rand.nextInt(10) + 20; // Richiede livello/EXP alto per essere affrontato!
+        int hp    = rand.nextInt(HP_RANGE)    + MIN_HP;
+        int atk   = rand.nextInt(ATK_RANGE)   + MIN_ATK;
+        int def   = rand.nextInt(DEF_RANGE)   + MIN_DEF;
+        int level = rand.nextInt(LEVEL_RANGE) + MIN_LEVEL;
 
         return new Boss(randomName, hp, atk, def, level);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Boss b)) return false;
+        return id != null && id.equals(b.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Boss{id=" + id + ", name='" + name + "', hp=" + hp + ", atk=" + atk + ", def=" + def + ", level=" + level + "}";
+    }
+
 }
